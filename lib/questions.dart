7f9890c770f6496ba060e5242d0bb1f14d1821d2
Ebrahim <morkasebrahim3@gmail.com>
2024-03-05 11:47:58 +0530

    Answers are being displayed and on click the questions are being changed and after last question when clicked on ans it shows index out of range

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,4 +1,4 @@
-// File that will display the questions oo screen
+// File that will display the questions on screen
 import "package:flutter/material.dart";
 import "package:quiz_app/questions_blueprint/questions_blueprint.dart";
 import 'package:quiz_app/data/questions_list.dart';
@@ -9,13 +9,30 @@ class Questions extends StatefulWidget {
 }
 
 class _QuestionsList extends State<Questions> {
+  var index = 0;
+  QuestionsBlueprint question = questions[0];
   void incrementIndex() {
-
+    
+  }
+  void answerClicked() {
+    if(index != questions.length-1)
+    {
+    setState(() {
+      index++;
+      question = questions[index];
+    });
+    // print(index);
+    // print("Button Clicked");
+    // question = questions[index];
+    // print(question.question);
+    }
+    else {
+      print("Index out of range");
+    }
   }
   Widget build(context) {
-    QuestionsBlueprint question = questions[1];
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.blue, Colors.purple],
         ),
@@ -24,6 +41,17 @@ class _QuestionsList extends State<Questions> {
         child: Column(
           children: [
             Text(question.question),
+            ...question.answersList.map((answer) {
+              return TextButton(
+                onPressed: answerClicked,
+                child: Text(answer),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purpleAccent,
+                  foregroundColor: Colors.white,
+                ),
+                );
+            }).toList(),
+            const SizedBox(height:80),
           ],
         ),
       ),
