9764e0d568ff7f201eaa425b40b03d13e1531f6a
Ebrahim <morkasebrahim3@gmail.com>
2024-03-05 19:17:05 +0530

    The users are being passed to the results page

--- lib/questions.dart
+++ lib/questions.dart
@@ -2,6 +2,7 @@
 import "package:flutter/material.dart";
 import "package:quiz_app/questions_blueprint/questions_blueprint.dart";
 import 'package:quiz_app/data/questions_list.dart';
+import 'package:quiz_app/results_page.dart';
 
 class Questions extends StatefulWidget {
   @override
@@ -10,22 +11,25 @@ class Questions extends StatefulWidget {
 
 class _QuestionsList extends State<Questions> {
   var index = 0;
+  List<String> userAnswers = [];
   QuestionsBlueprint question = questions[0];
   void answerClicked(buttonText) {
-    print(buttonText);
+    // print(buttonText);
+    userAnswers.add(buttonText);
     if(index != questions.length-1)
     {
     setState(() {
       index++;
       question = questions[index];
     });
-    // print(index);
-    // print("Button Clicked");
-    // question = questions[index];
     // print(question.question);
     }
     else {
-      print("Index out of range");
+      // print("Index out of range");
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ResultsPage(results:userAnswers))
+      );
     }
   }
   @override
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions_list.dart';
+import 'package:quiz_app/questions.dart';
+import 'package:quiz_app/questions_blueprint/questions_blueprint.dart';
 
 class ResultsPage extends StatefulWidget {
-    @override
+  final List<String> results;
+  ResultsPage({super.key, required this.results});
+
+  @override
   _DisplayResultsPage createState() => _DisplayResultsPage();
 }
 
 class _DisplayResultsPage extends State<ResultsPage> {
   @override
   Widget build(context) {
-    return Text("Hello World");
+    // Creating the list in which we will store the question and the answer given by user and all this will be in form of Map
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i <= widget.results.length; i++) {
+      summary.add({
+        "question": questions[i].question,
+        "correctAnswer": questions[i].answersList,
+        "userAnswer": widget.results[i]
+      });
+    }
+
+    return Container(
+        height: 100,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 67, 2, 78),
+            Color.fromARGB(255, 3, 9, 94)
+          ]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(widget.results[0]),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
